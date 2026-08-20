de4353e9e911b06ac21caf2be9358101a42711d3
jailine08 <jgbustamant@uncg.edu>
2026-08-20 16:38:19 -0400

    Data structure

---
+++ Script 1 data structure.R
@@ -0,0 +1,26 @@
+## scaler
+a <- 1.2
+b. <- "a"
+
+## vector
+va <- c(1.2, 3, 4.6)
+mean(va)
+
+mean
+
+## matrix
+vc <- c(1.3, 4.2, 5.6)
+vd <- c(10, 3, 2)
+
+
+
+
+x <- c("1", 3)
+
+
+## tricky data type: factor
+vz <- c("a", "b", "c")
+
+
+## data frame
+df0 <- data.frame(letter = vz, numeric = va)
